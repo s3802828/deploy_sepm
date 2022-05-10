7943a79a45e9d7c8c70da82676687fb4c2550f9a
Giang Le Quynh <s3802828@rmit.edu.vn>
2022-05-10 20:30:58 +0700

    changesssss

--- routes/profilePage/avatarRoute.js
+++ routes/profilePage/avatarRoute.js
@@ -2,7 +2,6 @@ var { updateAvatar } = require('../../controllers/ProfilePage/personalInfo');
 var router = require('express').Router();
 
 var multer = require('multer');
-
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './../../frontend/public/userUploads')
