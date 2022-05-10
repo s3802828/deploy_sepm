5a1914e629f8974e69d7bdd0249f3a3cf14fbaa0
Giang Le Quynh <s3802828@rmit.edu.vn>
2022-05-10 16:15:22 +0700

    deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer(app);
 const mongoose = require('mongoose');
 const MONGO_URI = "mongodb+srv://giangle:root@cheatsheet.l6veb.mongodb.net/test";
 
-const SERVER_PORT = 9000;
+const SERVER_PORT = process.env.PORT || 9000;
 mongoose
     .connect(MONGO_URI)
 
